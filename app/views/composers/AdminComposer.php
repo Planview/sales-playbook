f20091c7bc1a4a47a062a66769f2ada7a1efd472
Steve Crockett <crockett95@gmail.com>
2014-12-26 16:15:09 -0600

    Use Bootstrapper package to build sidebar

--- app/views/composers/AdminComposer.php
+++ app/views/composers/AdminComposer.php
@@ -5,6 +5,26 @@ class AdminComposer
     public function compose($view)
     {
         // cUser is short for current user
-        return $view->with('cUser', Confide::user());
+        return $view->with(
+            'sidebarMenu',
+            [
+                [
+                    'title' => 'Hello World',
+                    'link'  => URL::route('admin.dashboard')
+                ],
+                [
+                    'Users',
+                    [
+                        [
+                            'title' => 'All Users',
+                            'link'  => URL::route('admin.users.index')
+                        ],
+                        [
+                            'title' => 'Add New User',
+                            'link'  => URL::route('admin.users.create')
+                        ]
+                    ]
+                ]
+            ]);
     }
 }

8aa8b0d13e593de8d221e0ac0c08879478533790
unknown <sebner@AUCLPSLE188.us.planview.world>
2016-01-15 10:23:08 -0600

    Added new files for the 2016 Kickoff pages.

---
+++ public/js/kickoff-2016.js
@@ -0,0 +1,16 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery.min',
+        bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        }
+    }
+});
+require(['jquery', 'bootstrap'], function ($) {
+    'use strict';
+
+    $.noop();
+});
